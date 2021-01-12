7cbaf0ddcf5deffb749780c041dc36a98906a625
kangkung17 <mmuhdar17@gmail.com>
2021-01-12 12:46:24 +0700

    adding elements/form + proccess to detail page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import 'assets/scss/style.scss';
-import LandingPage from 'pages/LandingPage';
+import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
+import Example from "pages/Example";
+
+import "assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage}></Route>
+        <Switch>
+          <Route path="/" component={LandingPage} />
+          <Route path="/example" component={Example} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/utils/formatDate.js
@@ -0,0 +1,16 @@
+import Date from "elements/Form/InputDate";
+
+const formatDate = date => {
+  const d = new Date(date);
+  const dtf = new Intl.DateTimeFormat("en", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  });
+  const [{ value: mo }, , { value: da }] = dtf.formatToParts(d);
+
+  return `${da} ${mo}`;
+  //   return date;
+};
+
+export default formatDate;
